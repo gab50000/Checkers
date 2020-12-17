14d3181667bc7284f08764fd5124d80aa90a671d
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-12-17 23:14:00 +0100

    Add test for get_moves

--- tests/test_checkers.cpp
+++ tests/test_checkers.cpp
@@ -16,3 +16,19 @@ TEST(CheckersTest, TestMove) {
   EXPECT_EQ(new_board[0][0], BoardState::empty);
   EXPECT_EQ(new_board[1][1], BoardState::black_king);
 }
+
+TEST(CheckersTest, TestGetMoves) {
+  auto board = empty_board();
+  board[3][3] = BoardState::black_man;
+  board[2][2] = BoardState::white_man;
+
+  auto checkers = CheckersGame(board);
+
+  auto moves = checkers.get_moves();
+
+  EXPECT_EQ(moves.size(), 1);
+
+  auto* mv = static_cast<CheckersMove*>(moves[0].get());
+  auto target = std::make_pair(2, 2);
+  EXPECT_EQ(mv->from, target);
+}
