7b615e5c02e4941d3742f50c2756e79c366c1608
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-12-19 12:27:37 +0100

    Remove test

--- tests/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <checkers/board.cpp>
-#include <iostream>
-
-#include "gtest/gtest.h"
-
-TEST(BoardTest, TestSomething) {
-  auto board = empty_board();
-  ASSERT_EQ(board[0][0], BoardState::empty);
-}
